c23d4f42ac7babce789b9b41a5d6599802bc662c
Yianday <yianday0330h@gmail.com>
2026-03-26 01:36:57 +0800

    feat: Add PlayDetailPage for downloaded content playback

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -11,6 +11,7 @@ class Sizes {
   // Icon
   static const iconS = 16.0;
   static const iconM = 24.0;
+  static const iconXXL = 64.0;
 
   // Padding & Margin
   static const paddingXXS = 2.0;
@@ -18,8 +19,10 @@ class Sizes {
   static const paddingS = 8.0;
   static const paddingM = 12.0;
   static const paddingL = 16.0;
+  static const paddingXXL = 32.0;
 
   // Font
   static const fontSizeS = 12.0;
   static const fontSizeM = 16.0;
+  static const fontSizeXXL = 24.0;
 }
